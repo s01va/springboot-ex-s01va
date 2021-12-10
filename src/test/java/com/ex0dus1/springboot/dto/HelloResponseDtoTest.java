a529b781a493ffda4f8e9916a41be2d3cb90136d
s01va <jinn0525@gmail.com>
2021-12-10 14:08:38 +0900

    테스트 코드 작성/롬복 사용법 학습

---
+++ src/test/java/com/ex0dus1/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.ex0dus1.springboot.dto;
+
+import com.ex0dus1.springboot.web.dto.HelloResponseDto;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void lombok_funcTest() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
