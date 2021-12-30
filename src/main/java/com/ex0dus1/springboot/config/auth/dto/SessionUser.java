1d2c189d5efd30659b9689165f1e65d8b16756a9
s01va <jinn0525@gmail.com>
2021-12-30 16:59:35 +0900

    Oauth2.0 로그인 기능 구현

---
+++ src/main/java/com/ex0dus1/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.ex0dus1.springboot.config.auth.dto;
+
+import com.ex0dus1.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
